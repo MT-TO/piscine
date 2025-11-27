b5e166785e4154eebe1c00d565ed24768f34bb95
ludo <ludo.wilh@outlook.fr>
2025-11-27 16:28:37 +0100

    commande nage

--- piscine.py
+++ piscine.py
@@ -8,8 +8,7 @@ print("--- Gestionnaire d'utilisateurs d'une piscine ---")
 
 liste = []
 commande = ''
-#salut 
-# com MT 2 
+
 while commande != 'exit':
     commande = input("Que faut-il faire ? ")
 
@@ -17,10 +16,18 @@ while commande != 'exit':
         a = input("Qui nage ? ")
         b = input("quelle nage ? ")
         c = input("combien de longueur ? ")
-        liste.append((a,b,c))
-#salam
+        liste.append((a, b, c))
+
     if commande == 'liste':
         for elt in liste:
             print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
 
-# MT 
+    if commande == 'nage':
+        nage_recherchee = input("Quelle nage ? ")
+        trouve = False
+        for elt in liste:
+            if elt[1].lower() == nage_recherchee.lower():
+                print(f"{elt[0]} pratique cette nage ({elt[2]} longueurs)")
+                trouve = True
+        if not trouve:
+            print("Aucun nageur trouvé pour cette nage.")
